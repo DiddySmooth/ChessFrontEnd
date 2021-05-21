3cd6ac88bacaa345db3bf4e805e95c8a63e4fb5a
DiddySmooth <76023664+DiddySmooth@users.noreply.github.com>
2021-05-21 16:08:42 -0500

    heroku

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Redirect} from 'react-router-dom'
+import {useEffect, useContext} from 'react'
+import axios from 'axios'
+import {UserContext} from './Context/UserContext'
 import PrimarySearchAppBar from './Components/NavBar';
+import SignInSide from './Pages/LoginPage'
+
 function App() {
+  const {userState} = useContext(UserContext)
+  const [user, setUser] = userState
+  const userId = localStorage.getItem('userId')
+  const getUserInfo = async () => {
+    try {
+      let res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/user/info`, {
+        headers: {
+          Authorization: userId
+        }
+      })
+
+      if (res.data.user) {
+        setUser(res.data.user)
+        console.log(res.data.user)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => { getUserInfo() }, [])
   return (
     <div className="App">
       <PrimarySearchAppBar />
+
+      <Route exact path="/login">
+        {user ? 
+          <Redirect to="/home" />
+        :
+          <SignInSide />
+        }
+      </Route>
     </div>
   );
 }
