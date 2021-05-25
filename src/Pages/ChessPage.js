e4e8be9ff81ade9c80f97ebc6aa078fc362fca98
DiddySmooth <76023664+DiddySmooth@users.noreply.github.com>
2021-05-25 09:07:09 -0500

    sockets working

--- src/Pages/ChessPage.js
+++ src/Pages/ChessPage.js
@@ -1,17 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import socketIOClient from "socket.io-client";
 
 import WithMoveValidation from "./ChessIntergration";
 
-class Demo extends Component {
-  render() {
+const ENDPOINT = "http://127.0.0.1:3001";
+
+const Demo = () => {
+
+    const [response, setResponse] = useState("");
+
+    const socketConnect = () => {
+      const socket = socketIOClient(ENDPOINT, { transports: ['websocket', 'polling', 'flashsocket'] });
+      socket.on("FromAPI", data => {
+        setResponse(data);
+      });
+    }
     return (
       <div>
         <div style={boardsContainer}>
           <WithMoveValidation />
         </div>
+        <button onClick={ () => {socketConnect()}}>Click me</button>
       </div>
     );
-  }
 }
 
 export default Demo;
